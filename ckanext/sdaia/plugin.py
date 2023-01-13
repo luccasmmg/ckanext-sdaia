648da916bcbd6a1f254d4e5cb4da0046e7722a85
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2023-01-13 08:53:32 -0300

    feat: 02-chained-action

--- ckanext/sdaia/plugin.py
+++ ckanext/sdaia/plugin.py
@@ -2,6 +2,13 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from flask import Blueprint, render_template
 
+@toolkit.chained_action
+@toolkit.side_effect_free
+def package_search(original_action, context, data_dict):
+    result = original_action(context, data_dict)
+    result['Modified'] = True
+    return result
+
 def hello_world():
     '''A simple view function'''
     return "Hello World, this is my first ckan extension"
@@ -9,6 +16,7 @@ def hello_world():
 class SdaiaPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IBlueprint)
+    plugins.implements(plugins.IActions)
 
     # IConfigurer
     def update_config(self, config_):
@@ -31,3 +39,9 @@ class SdaiaPlugin(plugins.SingletonPlugin):
             methods=['GET']
         )
         return blueprint
+
+    #IActions
+    def get_actions(self):
+        return {
+            'package_search': package_search,
+        }
