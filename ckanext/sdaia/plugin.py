0b3d56338053ad36833244f292104c3c053a402c
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2023-01-13 09:36:53 -0300

    feat: 03-css

--- ckanext/sdaia/plugin.py
+++ ckanext/sdaia/plugin.py
@@ -22,9 +22,8 @@ class SdaiaPlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
+        toolkit.add_resource('assets',
             'sdaia')
-
     
     #IBlueprint
     def get_blueprint(self):
